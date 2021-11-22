ff862c066a0751bf09e96e44a8522cc7f821d0f3
Tyler Lafond <tlafo001@wch136-18.cs.ucr.edu>
2021-11-21 16:03:29 -0800

    Testing Joystick

--- FinalProject/source/main.c
+++ FinalProject/source/main.c
@@ -110,7 +110,6 @@ void tickDisplay() {
 			LCD_ClearScreen();
 		//	LCD_Cursor(1);
 			LCD_DisplayString(1, vSPD);
-			break;
 			if (vSpeed < 0) {
 				LCD_DisplayString(7, neg);
 				LCD_Cursor(8);
