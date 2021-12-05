304edcc55d24f98f19e93ead57afb55b8d1c9f41
Tyler Lafond <tlafo001@wch136-18.cs.ucr.edu>
2021-12-05 02:15:23 -0800

    Testing LCD

--- FinalProject/header/timer.h
+++ FinalProject/header/timer.h
@@ -1,5 +1,5 @@
 
-// Permission to copy is granted provided that this header remains intact. 
+// Permission to copy is granted provided that this header remains intact.
 // This software is provided with no warranties.
 
 ////////////////////////////////////////////////////////////////////////////////
