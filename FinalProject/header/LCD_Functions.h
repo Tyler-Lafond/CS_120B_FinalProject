b4bf5f4fd19f20f3a10632141b6a4e493d21702e
Tyler Lafond <tylerl5014@student.vvc.edu>
2021-12-06 00:16:03 -0800

    Fixed LCD_Functions.h

--- FinalProject/header/LCD_Functions.h
+++ FinalProject/header/LCD_Functions.h
@@ -187,23 +187,6 @@ byte displayMap[LCD_WIDTH * LCD_HEIGHT / 8] = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // (72,40)->(83,47) !!! The bottom right pixel!
 };
 
-/*
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val)
-{
-	uint8_t i;
-
-	for (i = 0; i < 8; i++) {
-		if (bitOrder == LSBFIRST)
-			digitalWrite(dataPin, !!(val & (1 << i)) );
-		else
-			digitalWrite(dataPin, !!(val & (1 << (7 - i))) );
-
-		digitalWrite(clockPin, HIGH);
-		digitalWrite(clockPin, LOW);
-			
-	}
-}
-*/
 // There are two memory banks in the LCD, data/RAM and commands.
 // This function sets the DC pin high or low depending, and then
 // sends the data byte
